bbfb90a408943e5d8ccd8c65da8bac6ca0976bc9
luizcarlos.dev <luizcarlos.junior100@gmail.com>
2022-02-27 16:21:19 -0300

    correçao

--- node/src/index.js
+++ node/src/index.js
@@ -14,32 +14,49 @@ function getConnection() {
 	return connection;
 }
 
-function inserirPeople() {
-	var date = new Date();
-	var dia = String(date.getDate()).padStart(2, '0');
-	var mes = String(date.getMonth() + 1).padStart(2, '0');
-	var ano = date.getFullYear();
-	var hora = String(date.getHours()).padStart(2, '0');
-	var minuto = String(date.getMinutes()).padStart(2, '0');
-	var segundo = String(date.getSeconds()).padStart(2, '0');
+function initDb() {
 
-	var dataAtual = dia + '/' + mes + '/' + ano;
+	let sql = `create table if not exists people(id int not null auto_increment, name varchar(255), primary key(id));`
+	let connection = getConnection();
+	connection.query(sql, function(err, results, fields) {
+		if (err) {
+		  console.log(err.message);
+		}
+	});
+	
+	connection.end(function(err) {
+		if (err) {
+		  return console.log(err.message);
+		}
+	});
+}
+
+function inserirPeople() {
+	let date = new Date();
+	let dia = String(date.getDate()).padStart(2, '0');
+	let mes = String(date.getMonth() + 1).padStart(2, '0');
+	let ano = date.getFullYear();
+	let hora = String(date.getHours()).padStart(2, '0');
+	let minuto = String(date.getMinutes()).padStart(2, '0');
+	let segundo = String(date.getSeconds()).padStart(2, '0');
+
+	let dataAtual = dia + '/' + mes + '/' + ano;
 	dataAtual += " " + hora + ":" + minuto + ":" + segundo;
 
-	var name = "Luiz Carlos - " + dataAtual;
-	const sql = `INSERT into people(name) values('${name}')`
-	var connection = getConnection();
+	let name = "Luiz Carlos - " + dataAtual;
+	let sql = `INSERT into people(name) values('${name}')`
+	let connection = getConnection();
 	connection.query(sql)
 	connection.end()
 	console.log("Inseriu Pessoa")
 }
 
 function getPeoples(req, res) {
-	const sql = `SELECT id, name FROM people ORDER BY id ASC`
+	let sql = `SELECT id, name FROM people ORDER BY id ASC`
 	let connection = getConnection();
 
 	connection.query(sql, function (err, rows) {
-		var resultado = '<h1>Full Cycle Rocks 2!</h1>';
+		let resultado = '<h1>Full Cycle Rocks 2!</h1>';
 
 		if (err) {
 			console.log("Erro ao buscar dados: " + err);
@@ -49,7 +66,7 @@ function getPeoples(req, res) {
 			let peoples = `<table style="width:50%"><tr><th align="left" style="width:30%">Código</th><th align="left" style="width:70%">Name</th></tr>`;
 			if (rows != null) {
 				Object.keys(rows).forEach(function(key) {
-					var row = rows[key];
+					let row = rows[key];
 					console.log(row.id + "-" + row.name)
 					peoples += `<tr><td align="left" style="width:30%">${row.id}</td><td align="left" style="width:730%">${row.name}</td></tr>`;
 				});
@@ -62,6 +79,7 @@ function getPeoples(req, res) {
 	});
 }
 
+initDb();
 inserirPeople();
 
 app.get('/', (req, res) => {
